2cca3e223813869e561d78688db61d166245b37e
Cristian Andres Velez Garcia <crisanbe289@gmail.com>
2022-12-10 08:04:14 -0500

    Se ajusta nueva version de la app versioncode 7

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -3,6 +3,6 @@ object ProjectConfig {
     const val compileSdk = 33
     const val minSdk = 21
     const val targetSdk = 33
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 7
+    const val versionName = "2.1"
 }
