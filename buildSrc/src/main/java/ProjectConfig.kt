558cf25e9799c0c8b72a740738ad8f0236da1a95
Cristian Andres Velez Garcia <crisanbe289@gmail.com>
2022-12-26 20:07:17 -0500

    Se ajusta url de produccion y nueva version

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -3,6 +3,6 @@ object ProjectConfig {
     const val compileSdk = 33
     const val minSdk = 21
     const val targetSdk = 33
-    const val versionCode = 12
-    const val versionName = "2.2.4"
+    const val versionCode = 13
+    const val versionName = "2.2.5"
 }
